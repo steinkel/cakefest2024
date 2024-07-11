2148dfb33ed3f5bf49dd859d2914c864df1f28a2
Jorge González <steinkel@gmail.com>
2024-07-11 10:52:25 +0100

    #save-columns add comma separated database type

---
+++ src/Database/Type/CommaSeparatedType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Type;
+
+use Cake\Database\Driver;
+use Cake\Database\Type\BaseType;
+
+class CommaSeparatedType extends BaseType
+{
+    public function toPHP(mixed $value, Driver $driver): mixed
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        return explode(',', $value);
+    }
+
+    public function marshal(mixed $value): mixed
+    {
+        if (is_array($value) || $value === null) {
+            return $value;
+        }
+
+        return explode(',', $value);
+    }
+
+    public function toDatabase(mixed $value, Driver $driver): mixed
+    {
+        return implode(',', $value);
+    }
+
+    public function toStatement(mixed $value, Driver $driver): int
+    {
+        if ($value === null) {
+            return \PDO::PARAM_NULL;
+        }
+
+        return \PDO::PARAM_STR;
+    }
+}
