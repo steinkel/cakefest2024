c147b9adf70bf4694dc1e9c35cdd8aa330ac33bd
Jorge González <steinkel@gmail.com>
2024-07-15 12:51:37 +0100

    improve display for column selection

--- plugins/ReportBuilder/templates/Reports/select_columns.php
+++ plugins/ReportBuilder/templates/Reports/select_columns.php
@@ -7,6 +7,7 @@ foreach ($report->all_columns as $associationName => $columns) {
         echo '<dl>';
         echo $this->Html->tag('dd', $this->Form->control(str_replace('.', ':', $associationName) . '.' . $column, [
             'type' => 'checkbox',
+            'label' => h($column),
         ]));
         echo '</dl>';
     }
